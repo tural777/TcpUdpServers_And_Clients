bfaeb2cfea75c397f20f4f9ed67cca497c75a62c
Tural Novruzov <turalinovruzov@gmail.com>
2020-07-26 17:00:17 +0400

    AddUdpServerAndUdpClient

---
+++ UDPClient/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace UDPClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+
+            var ipAddress = IPAddress.Loopback;
+            var port = 27001;
+
+            EndPoint endPoint = new IPEndPoint(ipAddress, port);
+
+            try
+            {
+                while (true)
+                {
+                    var text = Console.ReadLine();
+                    var bytes = Encoding.UTF8.GetBytes(text);
+
+                    socket.SendTo(bytes, endPoint);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ UDPServer/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace UDPServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var ipAddress = IPAddress.Any;
+            var port = 27001;
+
+            var socket = new Socket(AddressFamily.InterNetwork,
+                                    SocketType.Dgram,
+                                    ProtocolType.Udp);
+
+            var ipEndPoint = new IPEndPoint(ipAddress, port);
+
+            socket.Bind(ipEndPoint);
+
+            while (true)
+            {
+                var bytes = new byte[socket.ReceiveBufferSize];
+                EndPoint endPoint = new IPEndPoint(IPAddress.Any, 0); // butun ip adresler ve portlar gelen
+                var lenght = socket.ReceiveFrom(bytes, ref endPoint);
+
+                var msg = Encoding.UTF8.GetString(bytes, 0, lenght);
+                Console.WriteLine(msg);
+            }
+
+
+        }
+    }
+}
