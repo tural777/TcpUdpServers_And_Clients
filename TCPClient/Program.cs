55e2cbe570fa7f539d1eefab7f473bd8f3af00ed
Tural Novruzov <turalinovruzov@gmail.com>
2020-07-26 14:35:12 +0400

    Add project files.

---
+++ TCPClient/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace TCPClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var ipAddress = IPAddress.Parse("192.168.1.105"); // Your IP Address
+            var port = 27001;
+
+            var socket = new Socket(
+                AddressFamily.InterNetwork,
+                SocketType.Stream,
+                ProtocolType.Tcp);
+
+            var endPoint = new IPEndPoint(ipAddress, port);
+
+            try
+            {
+                socket.Connect(endPoint);
+
+                if (socket.Connected)
+                {
+                    Console.WriteLine("Connected to the server..");
+
+                    while (true)
+                    {
+                        var msg = Console.ReadLine();
+                        var bytes = Encoding.UTF8.GetBytes(msg);
+
+                        socket.Send(bytes);
+                    }
+
+                }
+                else
+                {
+                    Console.WriteLine("Can not connect to the server..");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Can not connect to the server..");
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ TCPServer/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TCPServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var ipAddress = IPAddress.Parse("192.168.1.105"); // Your IP Address
+            var port = 27001;
+
+            using (var socket = new Socket(
+                AddressFamily.InterNetwork,
+                SocketType.Stream,
+                ProtocolType.Tcp))
+            {
+
+                var endPoint = new IPEndPoint(ipAddress, port);
+
+                socket.Bind(endPoint);
+
+                var backlog = 10; // queue
+                socket.Listen(backlog);
+
+                while (true)
+                {
+
+                    Console.WriteLine($"Listening on {socket.LocalEndPoint}");
+                    var client = socket.Accept(); // Eger qoshulan yoxdusa novbeti setire kechmir.
+
+                    Task.Run(() =>
+                    {
+
+                        Console.WriteLine($"{client.RemoteEndPoint} connected...");
+
+                        var length = 0;
+                        var bytes = new byte[1024]; // 1024 bayt-bayt oxumasi uchun.
+
+                        do
+                        {
+
+                            length = client.Receive(bytes);
+                            var msg = Encoding.UTF8.GetString(bytes, 0, length);
+
+                            Console.WriteLine($"{client.RemoteEndPoint} : {msg}");
+
+                            if (msg == "ok")
+                            {
+                                client.Shutdown(SocketShutdown.Both);
+                                client.Dispose();
+                                break;
+                            }
+
+                        } while (true);
+
+
+                    });
+
+                }
+
+            }
+
+        }
+    }
+}
