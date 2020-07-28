abe64490e014790a10fc843c3c93df6575337fdb
Tural Novruzov <turalinovruzov@gmail.com>
2020-07-28 11:51:05 +0400

    WorkingFile_WithUdp

---
+++ WorkingWithFile_UdpClient/Client.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+
+namespace WorkingWithFile_UdpClient
+{
+    class Client
+    {
+        static void Main(string[] args)
+        {
+
+            string startupPath = Path.Combine(Directory.GetParent(System.IO.Directory.GetCurrentDirectory()).Parent.Parent.Parent.FullName);
+
+            string imagePath = startupPath + "/Images/uno.jpeg";
+
+            var bytes = ConvertImageToByteArray(imagePath);
+
+
+
+            var socket = new Socket(
+                AddressFamily.InterNetwork,
+                SocketType.Dgram,
+                ProtocolType.Udp);
+
+
+            var ipAddress = IPAddress.Parse("192.168.1.105");
+            var port = 27001;
+            var endPoint = new IPEndPoint(ipAddress, port);
+
+
+
+            // bytes.Length = 120769 byte
+            // udp ile ~65kb gondermek olur max.
+            socket.SendTo(bytes.Take(60000).ToArray(), endPoint);
+            socket.SendTo(bytes.Skip(60000).Take(60769).ToArray(), endPoint);
+
+
+            Console.WriteLine("Sent");
+
+
+            // Release the socket.
+            socket.Shutdown(SocketShutdown.Both);
+            socket.Close();
+        }
+
+
+        public static byte[] ConvertImageToByteArray(string imagePath)
+        {
+            byte[] imageByteArray = null;
+            FileStream fileStream = new FileStream(imagePath, FileMode.Open, FileAccess.Read);
+            using (BinaryReader reader = new BinaryReader(fileStream))
+            {
+                imageByteArray = new byte[reader.BaseStream.Length];
+                for (int i = 0; i < reader.BaseStream.Length; i++)
+                    imageByteArray[i] = reader.ReadByte();
+            }
+
+            return imageByteArray;
+        }
+    }
+}
---
+++ WorkingWithFile_UdpServer/Server.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+
+namespace WorkingWithFile_UdpServer
+{
+    class Server
+    {
+        static void Main(string[] args)
+        {
+            var ipAddress = IPAddress.Parse("192.168.1.105"); // Your IP Address
+            var port = 27001;
+
+            using (var socket = new Socket(
+                AddressFamily.InterNetwork,
+                SocketType.Dgram,
+                ProtocolType.Udp))
+            {
+
+                var endPoint = new IPEndPoint(ipAddress, port);
+                socket.Bind(endPoint);
+
+
+
+
+                var length = 0;
+                var buffer = new byte[66000];
+                List<byte> byteList = new List<byte>();
+                EndPoint endPointReceive = new IPEndPoint(IPAddress.Any, 0);
+
+
+
+
+                try
+                {
+
+                    int temp = 0;
+
+                    while (true)
+                    {
+                        Console.WriteLine("Waiting...");
+
+                        length = socket.ReceiveFrom(buffer, ref endPointReceive);
+
+                        Console.WriteLine($"length: {length}");
+
+
+                        for (int i = 0; i < length; i++)
+                        {
+                            byteList.Add(buffer[i]);
+                        }
+
+                        temp++;
+                        if (temp == 2) break;
+
+                    }
+
+
+
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+
+
+
+
+                Console.WriteLine("list count: " + byteList.Count);
+
+                // File writed
+                FileStream fs = File.Create("uno.jpeg");
+                fs.Write(byteList.ToArray());
+
+
+                socket.Shutdown(SocketShutdown.Both);
+                socket.Close();
+                Console.WriteLine("End");
+
+
+            }
+        }
+    }
+}
